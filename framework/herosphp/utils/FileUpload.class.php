5798ee319226d9a3d0aa0a4212c4d236f5ed19e9
yangjian <yangjian102621@gmail.com>
2014-11-18 17:39:39 +0800

    complete image processer code

--- framework/herosphp/utils/FileUpload.class.php
+++ framework/herosphp/utils/FileUpload.class.php
@@ -1,179 +1,260 @@
 <?php
+namespace common\utils;
+
 /**
  * 文件上传类, 支持多文件上传不重名覆盖。支持base64编码文件上传
- * file upload class, supports multi-file upload and base64-encoded file upload.
- * *******************************************************************************
- * 许可声明：此为专门为网络星空PHP高性能建站班级量身定制的"轻量级"PHP框架
- * *******************************************************************************
- * 版权所有 (C) 2013.03-now 网络星空工作室研发中心 并保留所有权利。           	
  * @author 	yangjian<yangjian102621@gmail.com>
- * @version  1.3
- * @completed 	2013.04.11
- * @last-update	2013.04.11
+ * @version  1.4
+ * @since	2014.04.11
  * ******************************************************************************/
 class FileUpload {
-	
-	/* file upload configs */
-	private $config = NULL;
-	/* max filesize of upload default : 2*1024*1024  */
-	private static $Max_fsize = 2097152;
-	
-	/* error number of file upload */
-	private $err_num = 0;
-	/* error message of file upload */
-	private static $err_msg = array();
-	/* upload state info (上传状态映射表，国际化用户可在此添加国际化语言) */
-	private static $stataInfoMap = array(
-		 'ch' => array(		
-		 	  '0'		=> 	'文件上传成功', 
-		 	  '1'		=> 	'文件超出大小限制', 
-		 	  '2'		=> 	'文件的大小超过了HTML表单指定值', 
-		 	  '3'		=> 	'文件只有部分被上传',
-		 	  '4'		=> 	'文件没有被上传',
-		 	  '5'		=> 	'不允许的文件类型',
-		 	  '6'		=> 	'上传目录创建失败',
-		 	  '7'		=> 	'文件保存时出错',
-		 	  '8'		=> 	'base64解码IO错误',
-		 	  '9'		=>  '未知错误'
-		 	),
-		 'en' => array(		
-		 	  '0'		=> 	'File uploaded successfully!', 
-		 	  '1'		=> 	'File size exceeds the limit', 
-		 	  '2'		=> 	'File size exceeds the limit of HTML',
-		 	  '3'		=> 	'File is not completed uploaded.',
-		 	  '4'		=> 	'File is not uploaded',
-		 	  '5'		=> 	'Disallowed file types',
-		 	  '6'		=> 	'Failed to make the upload directory',
-		 	  '7'		=> 	'Failed to save the uploaded file.',
-		 	  '8'		=> 	'base64 encode IO erro',
-		 	  '9'		=>  'Unknow error!'
-		 	),
 
+    /**
+     * @var array 上传文件配置参数
+     */
+    protected $config = array(
+        'upload_dir' => __DIR__,
+        'allow_ext' => 'jpg|png|gif',
+        'max_size' =>  2097152,     /* 文件的最大尺寸默认 2MB */
+     );
+
+    /**
+     * @var array 上传文件的信息
+     * local_name => 上传文件的客户端名称
+     * file_size => 文件大小
+     * file_name => 上传文件的新的文件名
+     * file_ext => 上传文件的后缀名称
+     * file_type => 本次上传的mine类型
+     * is_image => 是否是图片
+     * image_width => 图片宽度
+     * image_height => 图片高度
+     * image_size_str => 图片尺寸字符串 width="396" height="341"
+     * file_path => 文件的绝对路径
+     * file_name => 文件名(带后缀)
+     * file_ext => 文件后缀
+     * raw_name => 文件名(不带后缀)
+     */
+    protected $fileInfo = array();
+
+    /**
+     * @var string 上传文件的后缀名
+     */
+    protected $extension = '';
+
+    /**
+     * @var int 上传的错误代码
+     */
+    protected $errNum = 0;
+
+    /**
+     * @var array 上传的状态信息
+     */
+    protected static $_UPLOAD_STATES = array(
+        '0'		=> 	'文件上传成功',
+        '1'		=> 	'文件超出大小限制',
+        '2'		=> 	'文件的大小超过了HTML表单指定值',
+        '3'		=> 	'文件只有部分被上传',
+        '4'		=> 	'文件没有被上传',
+        '5'		=> 	'不允许的文件类型',
+        '6'		=> 	'上传目录创建失败',
+        '7'		=> 	'文件保存时出错',
+        '8'		=> 	'base64解码IO错误'
 	);
 
-	/**
-	 * constructor
-	 * @param 		array()			$_config   
-	 * @notice 		$_config keys  upload_dir, allowExt, max_szie
-	 * @param 		boolean 		$_base64   is base64 file stream		
-	 */
-	public function __construct( &$_config ) {
-		if ( !isset($_config) ) {
-			trigger_error('Must pass in parameters $_config');
-			return FALSE;
-		}
-		$this->config = $_config;
-		if ( isset($_config['max_size']) ) self::$Max_fsize = $_config['max_size'];
-		ini_set('upload_max_filesize', ceil(self::$Max_fsize/(1024*1024)).'M');
+    /**
+     * constructor
+     * @param        array() $_config
+     * @notice        $_config keys  upload_dir, allowExt, max_szie
+     */
+	public function __construct( $_config ) {
+
+		if ( !isset($_config) ) return FALSE;
+
+		$this->config = array_merge($this->config, $_config);
+		ini_set('upload_max_filesize', ceil( $this->config['max_size'] / (1024*1024) ).'M');
 	}
 
-	/**
-	 * upload file method.
-	 * @param 		sting 		$_field    	name of form elements.
-	 * @return 		string 		$_filename     filename string of uploaded file.
-	 */
+    /**
+     * upload file method.
+     * @param        sting $_field name of form elements.
+     * @param        bool $_base64
+     * @return       mixed        $_filename     filename string of uploaded file.
+     */
 	public function upload( $_field, $_base64 = false ) {
+		
 		if ( $_base64 ) {
 			$_data = $_POST[$_field];
 			return $this->makeBase64Image( $_data );
 		}
-		if ( !$this->check_upload_dir() ) {
-			$this->err_num = 6;
-			$this->addMessage($this->err_num, $this->$this->config['upload_dir']);
-			return FALSE;
+		
+		if ( !$this->checkUploadDir() ) {
+			$this->errNum = 6;
+			return false;
 		}
-		$_localFile = is_array($_FILES[$_field]['name']) ? $_FILES[$_field]['name'] : array($_FILES[$_field]['name']);
-		$_tempFile = is_array($_FILES[$_field]['tmp_name']) ? $_FILES[$_field]['tmp_name'] : array($_FILES[$_field]['tmp_name']);
-		$_error_no = is_array($_FILES[$_field]['error']) ? $_FILES[$_field]['error'] : array($_FILES[$_field]['error']);
-		$_file_num = count($_localFile);
-		$_filename = '';
-		for ( $i = 0; $i < $_file_num; ++$i ) {
-			$this->err_num = $_error_no[$i];
-			if ( $this->err_num == 0 ) {
-				$this->check_file_type($_localFile[$i]);
-				if ( $this->err_num == 0 ) {
-					$this->check_file_size($_tempFile[$i]);
-					if ( $this->err_num == 0 ) {
-						if ( is_uploaded_file($_tempFile[$i]) ) {
-							$_new_filename = $this->getFileName($_localFile[$i]);
-							if ( move_uploaded_file($_tempFile[$i], $this->config['upload_dir'].DIR_OS.$_new_filename) ) {
-								$_filename .= $_filename == NULL ? $_new_filename : ','.$_new_filename;
-							} else {
-								$this->err_num = 7;
-							}
-						}
-					}
-				}
-			}
-			$this->addMessage($this->err_num, $_localFile[$i]);
+		
+		$_localFile = $_FILES[$_field]['name'];
+		$_tempFile = $_FILES[$_field]['tmp_name'];
+		$_error_no = $_FILES[$_field]['error'];
+        $this->fileInfo['client_name'] = $_localFile;
+        $this->fileInfo['file_size'] = floatval($_FILES[$_field]['size'] / 1024);
 
-		}
+		$_filename = '';
+        $this->errNum = $_error_no;
+        if ( $this->errNum == 0 ) {
+            $this->checkFileType($_localFile);
+            if ( $this->errNum == 0 ) {
+                $this->checkFileSize($_tempFile);
+                if ( $this->errNum == 0 ) {
+                    if ( is_uploaded_file($_tempFile) ) {
+                        $_new_filename = $this->getFileName($_localFile);
+                        $this->fileInfo['file_path'] = $this->config['upload_dir'].DIRECTORY_SEPARATOR.$_new_filename;
+                        if ( move_uploaded_file($_tempFile, $this->fileInfo['file_path']) ) {
+
+                            $_filename = $_new_filename;
+                            $this->fileInfo['file_name'] = $_filename;
+                            $pathinfo = pathinfo($this->fileInfo['file_path']);
+                            $this->fileInfo['file_ext'] =  $pathinfo['extension'];
+                            $this->fileInfo['raw_name'] = $pathinfo['filename'];
+
+                        } else {
+                            $this->errNum = 7;
+                        }
+                    }
+                }
+            }
+        }
 		return $_filename;
-		//Utils::myPrint($_file);
 
 	}
 
 	/**
-	 * make base64 image, convert base64 code to image
+	 * 接收base64位参数，转存图片
 	 */
-	private function makeBase64Image( $_base64_data ) {
+	protected function makeBase64Image( $_base64_data ) {
+
 		$_img = base64_decode($_base64_data);
 		$_filename = time().rand( 1 , 1000 ).".png";
-		if ( file_put_contents($this->config['upload_dir'].DIR_OS.$_filename, $_img) ) {
+		if ( file_put_contents($this->config['upload_dir'].DIRECTORY_SEPARATOR.$_filename, $_img) ) {
 			return $_filename;
 		}
-		$this->err_num = 8;
-		$this->addMessage($this->err_num, 'base64 data save faild. '.$_filename);
+		$this->errNum = 8;
+
 	}
 
-	/* get new filename */
-	private function getFileName($filename) {
-		$_ext = Utils::getFileExt($filename);
+    /**
+     * 获取新的文件名
+     * @param $filename
+     * @return string
+     */
+    protected function getFileName($filename) {
+
+		$_ext = $this->getFileExt($filename);
 		list($msec, $sec) = explode(' ', microtime());
 		return $sec.'-'.substr($msec, 2, 2).'-'.mt_rand(1000, 9999).'.'.$_ext;
+
 	}
 
-	/* check upload dir */
-	private function check_upload_dir() {
+    /**
+     * 检测上传目录
+     * @return bool
+     */
+    protected function checkUploadDir() {
 		if ( !file_exists($this->config['upload_dir']) ) {
-			return Utils::makeFileDirs($this->config['upload_dir']);
+			return self::makeFileDirs($this->config['upload_dir']);
 		}
-		return TRUE;
+		return true;
 	}
 
-	/* check file type */
-	private function check_file_type( $filename ) {
-		$_ext = Utils::getFileExt($filename);
-		if ( !in_array($_ext, $this->config['allowExt']) ) {
-			$this->err_num = 5;
+    /**
+     * 创建多级目录
+     * @param $path
+     * @return bool
+     */
+    public static function makeFileDirs( $path ) {
+        //必须考虑 "/" 和 "\" 两种目录分隔符
+        $files = preg_split('/[\/|\\\]/s', $path);
+        $_dir = '';
+        foreach ($files as $value) {
+            $_dir .= $value.DIRECTORY_SEPARATOR;
+            if ( !file_exists($_dir) ) {
+                mkdir($_dir);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 检测文件类型是否合法
+     * @param $filename
+     */
+    protected function checkFileType( $filename ) {
+
+		$_ext = self::getFileExt($filename);
+        $_allow_ext = explode("|", $this->config['allow_ext']);
+		if ( !in_array($_ext, $_allow_ext) ) {
+			$this->errNum = 5;
 		}
 	}
 
-	/* check filesize  */
-	private function check_file_size( $filename ) {
-		if ( filesize($filename) > self::$Max_fsize ) 
-			$this->err_num = 1;
+    /**
+     * 获取文件名后缀
+     * @param $filename
+     * @return string
+     */
+    protected function getFileExt( $filename ) {
+        $_pos = strrpos( $filename, '.' );
+        return strtolower( substr( $filename , $_pos+1 ) );
+    }
+
+    /**
+     * 检查文件大小是否合格
+     * @param $filename
+     */
+    protected function checkFileSize( $filename ) {
+
+        if ( filesize($filename) > $this->config['max_size'] ) {
+            $this->errNum = 1;
+        }
+
+        if ( function_exists('finfo_open') ) {
+            $finfo = finfo_open(FILEINFO_MIME);
+            $this->fileInfo['file_type'] = finfo_file($finfo, $filename);
+        } else if ( function_exists('mime_content_type') ) {
+            $this->fileInfo['file_type'] = mime_content_type($filename);
+        }
+
+        if ( strpos($this->fileInfo['file_type'], 'image') !== FALSE ) {
+
+            $this->fileInfo['is_image'] = 1;
+            $size = getimagesize($filename);
+            if ( ($this->config['max_width'] > 0 && $size[0] > $this->config['max_width'])
+                || ($this->config['max_height'] > 0 && $size[1] > $this->config['max_height']) )  {
+                $this->errNum = 9;
+            }
+            $this->fileInfo['image_width'] = $size[0];
+            $this->fileInfo['image_height'] = $size[1];
+            $this->fileInfo['image_size_str'] = $size[3];
+
+        } else {
+
+            $this->fileInfo['is_image'] = 0;
+        }
 	}
 
-	/**
-	 * add message to error message array
-	 * @param 		string 		$_error_no     error number
-	 * @param  		string 		$_msg			error message
-	 */
-	private function addMessage( $_error_no, $_msg ) {
-		self::$err_msg[$_msg] = $_error_no;
-	}
+    /**
+     * 获取上传文件信息
+     */
+    public function getFileInfo() {
 
-	/**
-	 * get upload message
-	 * @param 		string  		$_lang     错误信息的语言
-	 */
-	public function get_upload_messgae( $_lang = 'ch' ) {
-		$_err_arr = array();
-		foreach ( self::$err_msg as $_key => $_val )  {
-			$_err_arr[] = "错误: {$_key}, ".self::$stataInfoMap[$_lang][$_val];
-		}
-		return $_err_arr;
+        return $this->fileInfo;
+    }
+
+    /**
+     * get upload message
+     * @return       string
+     */
+	public function getUploadMessage() {
+		return self::$_UPLOAD_STATES[$this->errNum];
 	}
 	
 }
---
+++ framework/herosphp/utils/ImageThumb.class.php
@@ -0,0 +1,272 @@
+<?php
+
+namespace common\utils;
+
+/**
+ * Class ImageThumb 缩略图生成类。支持jpg, png, gif格式的图片
+ * @author yangjian<yangjian102621@gmail.com>
+ * @since version 1.2 2014-10-13
+ */
+class ImageThumb {
+
+    /**
+     * @var ImageThumb 图片处理的唯一实例
+     */
+    protected static $_INSTANCE = NULL;
+
+    /**
+     * @var 原图resource
+     */
+    protected $imgSrc = NULL;
+
+    /**
+     * @var 目标图片resource
+     */
+    protected $imgDst = NULL;
+
+    /**
+     * @var array 原图的尺寸
+     */
+    protected $sizeSrc = array();
+
+    /**
+     * @var array 缩放目标图片的尺寸
+     */
+    protected $sizeDst = array();
+
+    /**
+     * @var string 图片的后缀
+     */
+    protected $extension = 'jpg';
+
+    /**
+     * @var int 缩放方式
+     * 0 => 直接缩放到目标大小(默认)
+     * 1 => 等比缩放到指定的size
+     * 2 => 指定宽度或者高度，另一维度按照等比缩放, 必须指定某一维度，而另一维度的值为0
+     * array(0, 700) 高度最大700, 宽度等比缩放
+     * array(700, 0) 宽度最大700,高对等比缩放
+     *
+     */
+    protected $flag = 0;
+
+    /**
+     * 私有化构造方法
+     */
+    private function __construct() {}
+
+    /**
+     * 获取缩略图的唯一实例
+     * @return ImageThumb
+     */
+    public static function getInstance() {
+
+        if (self::$_INSTANCE == NULL)
+            self::$_INSTANCE = new self();
+        return self::$_INSTANCE;
+    }
+
+    /**
+     * 生成缩略图
+     * @param     array $size new size of image
+     * @param     string $imgSrc Original image file path
+     * @param    int $quality 图片质量(0-100)
+     * @return  mixed
+     */
+    public function makeThumb($size, $imgSrc, $quality=75)
+    {
+        $this->extension = $this->getFileExt($imgSrc);
+        $this->sizeSrc = $this->getImageSize($imgSrc);
+        $this->imgDst = $this->createDstImage($size); //创建目标图像资源
+
+        //目标图片的拷贝
+        $this->imgSrc = $this->getImageSource($imgSrc); //获取图片资源
+        if ($this->imgSrc && $this->imgDst) {
+            $result = imagecopyresampled($this->imgDst, $this->imgSrc, 0, 0, 0, 0, $this->sizeDst[0],
+                $this->sizeDst[1], $this->sizeSrc[0], $this->sizeSrc[1]);
+
+            if (!$result) return false;
+        }
+
+        $thumbImg = $this->getThumbFilename($imgSrc, $size);
+        $this->saveImage($thumbImg, $quality); //图像输出,保存
+
+        return $thumbImg;
+    }
+
+    /**
+     * sava image
+     * @param    string $filename 保存新的文件名称
+     * @param    int $quality 图片质量，仅对jpeg图像有效
+     * @return  boolean
+     */
+    public function saveImage($filename, $quality = 75) {
+
+        switch ( $this->extension ) {
+
+            case 'jpg':
+            case 'jpeg':
+                return imagejpeg($this->imgDst, $filename, $quality);
+
+            case 'gif':
+                return imagegif($this->imgDst, $filename);
+
+            case 'png':
+                return imagepng($this->imgDst, $filename);
+
+        }
+
+        return false;
+    }
+
+    /**
+     * 输出图片
+     */
+    public function showImage() {
+
+        header("Content-Type:image/".$this->extension);
+        imagejpeg($this->imgDst, NULL);
+    }
+
+    /**
+     * 获取缩略图的名称
+     * @param $filename 文件名称
+     * @param $size 缩放尺寸
+     * @return array
+     */
+    protected function getThumbFilename($filename, $size)
+    {
+        return $filename . ".{$size[0]}x{$size[1]}." . $this->extension;
+    }
+
+    /**
+     * 获取文件后缀名
+     * @param $filename
+     * @return string
+     */
+    protected function getFileExt($filename) {
+
+        $info = pathinfo($filename);
+        return $info['extension'];
+
+    }
+
+    /**
+     * 获取图片尺寸
+     * @param $filename
+     * @return array
+     */
+    protected function getImageSize($filename)
+    {
+        $info = getimagesize($filename);
+        return array($info[0], $info[1]);
+    }
+
+    /**
+     * 创建目标图片
+     * @param array $size 缩放尺寸
+     * @return midex
+     */
+    protected function createDstImage($size)
+    {
+        switch ( $this->flag ) {
+            //直接缩放
+            case 0:
+                $this->sizeDst = $size;
+                break;
+            //等比缩放到指定size
+            case 1:
+                $_ratio = $this->getZoomRatio($size); //获取缩放比例
+                if ($_ratio == 0) break;
+                $this->sizeDst = $this->getDstImageSize($_ratio);
+                break;
+            //规定高|宽, 等比缩放
+            case 2:
+                $_ratio = max($size[0] / $this->sizeSrc[0], $size[1] / $this->sizeSrc[1]);
+                if ($_ratio == 0) break;
+                $this->sizeDst[0] = intval($this->sizeSrc[0] * $_ratio);
+                $this->sizeDst[1] = intval($this->sizeSrc[1] * $_ratio);
+                break;
+        }
+
+        return imagecreatetruecolor($this->sizeDst[0], $this->sizeDst[1]);
+    }
+
+    /**
+     * get image resource.(获取图片资源)
+     * @param   string $filename 图片路径
+     * @return  null|resource
+     */
+    protected function  &getImageSource($filename) {
+
+        $img = NULL;
+        $extension = $this->getFileExt($filename);
+        switch ( $extension ) {
+
+            case 'gif':
+                $img = imagecreatefromgif($filename);
+                break;
+
+            case 'png':
+                $img = imagecreatefrompng($filename);
+                break;
+
+            case 'jpg':
+            case 'jpeg':
+                $img = imagecreatefromjpeg($filename);
+
+        }
+        return $img;
+    }
+
+    /**
+     * 计算出目标图片的尺寸。
+     * 当图片缩放后的大小比画布小时，图片居中，多余的补白
+     * @param    float $ratio 缩放比率
+     * @return array
+     */
+    protected function getDstImageSize($ratio) {
+
+        return array(floor($this->sizeSrc[0] * $ratio), floor($this->sizeSrc[1] * $ratio));
+
+    }
+
+    /**
+     * 获取图片缩放比率
+     * @param  array $size 目标图片画布大小
+     * @return mixed
+     */
+    protected function getZoomRatio($size) {
+
+        return min($size[0] / $this->sizeSrc[0], $size[1] / $this->sizeSrc[1]);
+    }
+
+    /**
+     * @param int $flag
+     */
+    public function setFlag($flag)
+    {
+        $this->flag = $flag;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFlag()
+    {
+        return $this->flag;
+    }
+
+    /**
+     * destroy image resource(销毁图片资源，释放内存)
+     */
+    public function __distruct() {
+
+        if ( $this->imgSrc )
+            imagedestroy($this->imgSrc);
+
+        if ( $this->imgDst )
+            imagedestroy($this->imgDst);
+    }
+
+}
