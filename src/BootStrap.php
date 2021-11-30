e0da2de8cf2a3b6f9cef38f1977fb007912c660c
yangjian <yangjian102621@gmail.com>
2021-11-30 16:22:06 +0800

    optimize the cli run mode

--- src/BootStrap.php
+++ src/BootStrap.php
@@ -5,10 +5,11 @@
  * @author yangjian<yangjian102621@gmail.com>
  * @since v1.0.0
  */
+
 namespace herosphp;
 
 // 检测PHP环境
-if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+if (version_compare(PHP_VERSION, '5.3.0', '<')) die('require PHP > 5.3.0 !');
 define('FRAME_VERSION', '3.0.0'); //框架版本
 date_default_timezone_set(TIME_ZONE); //设置默认时区
 
@@ -17,24 +18,30 @@ use herosphp\core\WebApplication;
 use herosphp\api\GeneralApi;
 use herosphp\api\RestfulApi;
 
-class BootStrap {
+class BootStrap
+{
 
     /**
      * 框架启动入口函数
      */
-    public static function run() {
-
+    public static function run()
+    {
         //根据环境配置来获取相应的配置
-        $appConfigs = Loader::config('app', 'env.'.ENV_CFG);
+        $appConfigs = Loader::config('app', 'env.' . ENV_CFG);
         $application = WebApplication::getInstance();
         $application->execute($appConfigs);
-
     }
 
     /**
      * 客户端入口
      */
-    public static function artisan() {
-        Artisan::run();
+    public static function runCli($argv, $argc)
+    {
+        error_reporting(E_ERROR);
+        $params = getCliArgs($argv, $argc);
+        if ($params['debug'] == 'true') {
+            $_SERVER['debug'] = true;
+        }
+        Artisan::run($params);
     }
 }
--- src/functions.core.php
+++ src/functions.core.php
@@ -11,22 +11,23 @@ use herosphp\exception\HeroException;
 /**
  * 打印函数, 打印变量(数据)
  */
-function __print() {
+function __print()
+{
     $_args = func_get_args();  //获取函数的参数
 
-    if( count($_args) < 1 ) {
+    if (count($_args) < 1) {
         trigger_error('必须为myprint()参数');
         return;
     }
 
     echo '<div style="width:100%;text-align:left"><pre>';
     //循环输出参数
-    foreach( $_args as $_a ){
-        if( is_array($_a) ){
+    foreach ($_args as $_a) {
+        if (is_array($_a)) {
             print_r($_a);
             echo '<br />';
-        } else if( is_string($_a) ){
-            echo $_a.'<br />';
+        } else if (is_string($_a)) {
+            echo $_a . '<br />';
         } else {
             var_dump($_a);
             echo '<br />';
@@ -39,17 +40,19 @@ function __print() {
  * 打印一行
  * @param $msg
  */
-function printLine($msg) {
-    echo ("{$msg} \n");
+function printLine($msg)
+{
+    echo("{$msg} \n");
 }
 
 /**
  * 终端高亮打印绿色
  * @param $message
  */
-function tprintOk( $message ) {
+function tprintOk($message)
+{
 
-   printf("\033[32m\033[1m{$message}\033[0m\n");
+    printf("\033[32m\033[1m{$message}\033[0m\n");
 
 }
 
@@ -57,7 +60,8 @@ function tprintOk( $message ) {
  * 终端高亮打印红色
  * @param $message
  */
-function tprintError( $message ) {
+function tprintError($message)
+{
 
     printf("\033[31m\033[1m{$message}\033[0m\n");
 
@@ -67,7 +71,8 @@ function tprintError( $message ) {
  * 终端高亮打印黄色
  * @param $message
  */
-function tprintWarning( $message ) {
+function tprintWarning($message)
+{
 
     printf("\033[33m\033[1m{$message}\033[0m\n");
 
@@ -75,10 +80,11 @@ function tprintWarning( $message ) {
 
 /**
  * 计算字符串的hash值
- * @param string  $str
+ * @param string $str
  * @return int
  */
-function getHashCode( $str ) {
+function getHashCode($str)
+{
 
     return \herosphp\utils\HashUtils::JSHash($str);
 
@@ -90,16 +96,18 @@ function getHashCode( $str ) {
  * @param int $code
  * @throws herosphp\exception\HeroException
  */
-function E( $message, $code=0 ) {
+function E($message, $code = 0)
+{
     $exception = new HeroException($message, $code);
     throw $exception;
 }
 
 /**
  * 获取当前时间
- * @return 		int
+ * @return        int
  */
-function timer() {
+function timer()
+{
     list($msec, $sec) = explode(' ', microtime());
     return ((float)$msec + (float)$sec);
 }
@@ -109,52 +117,53 @@ function timer() {
  * @param $url
  * @return string
  */
-function url($url) {
+function url($url)
+{
 
     $url = ltrim($url, '/');
     $args = '';
     $urlInfo = parse_url($url);
 
-    if ( $urlInfo['path'] && $urlInfo['path'] != '/' ) {
+    if ($urlInfo['path'] && $urlInfo['path'] != '/') {
         $filename = str_replace(EXT_URI, '', $urlInfo['path']);
         $filename = rtrim($filename, "/");
         $pathInfo = explode('/', $filename);
         //提取pathinfo参数
         $paramArr = array();
-        if ( count($pathInfo) > 3 ) {
-            if ( isset($pathInfo[3]) ) {
+        if (count($pathInfo) > 3) {
+            if (isset($pathInfo[3])) {
                 $params = explode('-', $pathInfo[3]);
-                for ( $i = 0; $i < count($params); $i++ ) {
-                    if ( $i % 2 == 0 ) {
-                        if ( trim($params[$i]) == ''
-                            || trim($params[$i+1]) == ''
-                            || strpos($params[$i], '=') != false ) {
+                for ($i = 0; $i < count($params); $i++) {
+                    if ($i % 2 == 0) {
+                        if (trim($params[$i]) == ''
+                            || trim($params[$i + 1]) == ''
+                            || strpos($params[$i], '=') != false) {
                             continue;
                         }
                         $paramArr[] = $params[$i];
-                        $paramArr[] = $params[$i+1];
+                        $paramArr[] = $params[$i + 1];
                     }
                 }
             }
         }
 
         //获取常规参数
-        if ( $urlInfo['query'] ) {
+        if ($urlInfo['query']) {
             $query = preg_replace('/[&|=]/', '-', $urlInfo['query']);
-            if ( $query ) $args .= $args == '' ? $query : '-'.$query;
+            if ($query) $args .= $args == '' ? $query : '-' . $query;
         }
 
         $newUrl = "/{$pathInfo[0]}/{$pathInfo[1]}/{$pathInfo[2]}";
-        if ( !empty($paramArr) ) {
-            $newUrl .= '/'.implode('-', $paramArr);
+        if (!empty($paramArr)) {
+            $newUrl .= '/' . implode('-', $paramArr);
         }
 
-        if ( trim($args) != '' ) $newUrl .= '/'.$args;
+        if (trim($args) != '') $newUrl .= '/' . $args;
         $newUrl .= EXT_URI;
-        $newUrl = rtrim($newUrl,'/');
+        $newUrl = rtrim($newUrl, '/');
         return $newUrl;
     }
-    return '/'.$url;    //短链接
+    return '/' . $url;    //短链接
 
 }
 
@@ -163,32 +172,33 @@ function url($url) {
  * @param $url
  * @return string
  */
-function getSourceUrl($url) {
+function getSourceUrl($url)
+{
 
-    if ( strpos($url, '?') !== false ) {
+    if (strpos($url, '?') !== false) {
         return $url;
     }
     $url = trim($url, '/');
     $url = str_replace(EXT_URI, '', $url);
     //search / number
     $snum = mb_substr_count($url, '/');
-    if ( $snum < 3 ) {
-        return '/'.$url;
+    if ($snum < 3) {
+        return '/' . $url;
     } else {
         $pos = strrpos($url, '/');
-        $query = substr($url, $pos+1);
+        $query = substr($url, $pos + 1);
         $params = explode('-', $query);
-        $url = substr($url, 0, $pos+1).'?';
+        $url = substr($url, 0, $pos + 1) . '?';
         $length = count($params);
         $args = array();
-        for ( $i = 0; $i < $length-1; $i++ ) {
-            if ( $i % 2 == 0 ) {
-                $args[$params[$i]] = $params[$i+1];
+        for ($i = 0; $i < $length - 1; $i++) {
+            if ($i % 2 == 0) {
+                $args[$params[$i]] = $params[$i + 1];
             }
         }
-        return '/'.$url.http_build_query($args);
+        return '/' . $url . http_build_query($args);
     }
-    return '/'.$url;
+    return '/' . $url;
 
 }
 
@@ -199,29 +209,30 @@ function getSourceUrl($url) {
  * @param $key
  * @return string
  */
-function removeUrlArgs( $url, $key ) {
+function removeUrlArgs($url, $key)
+{
 
     $url = trim($url, '/');
     $pos = strrpos($url, '/');
-    if ( $pos === FALSE ) return $url;
+    if ($pos === FALSE) return $url;
 
     //移除后缀
     $url = str_replace(EXT_URI, '', $url);
-    $prefix = substr($url, 0, $pos+1);
+    $prefix = substr($url, 0, $pos + 1);
     //移除所有的参数，只留下root url
-    if ( $key == 'all' ) {
+    if ($key == 'all') {
         return $prefix;
     } else {
-        $args_str = substr($url, $pos+1);
+        $args_str = substr($url, $pos + 1);
         $args = explode('-', $args_str);
         $length = count($args);
-        for ( $i = 0; $i < $length; $i++ ) {
-            if ( $args[$i] == $key ) {
+        for ($i = 0; $i < $length; $i++) {
+            if ($args[$i] == $key) {
                 unset($args[$i]);
-                unset($args[$i+1]);
+                unset($args[$i + 1]);
             }
         }
-        return '/'.$prefix.implode('-', $args);
+        return '/' . $prefix . implode('-', $args);
     }
 }
 
@@ -232,16 +243,17 @@ function removeUrlArgs( $url, $key ) {
  * @param $value
  * @return string
  */
-function addUrlArgs($url, $key, $value) {
+function addUrlArgs($url, $key, $value)
+{
 
     $url = trim($url, '/');
     $pos = strrpos($url, '/');
     //移除后缀
     $url = str_replace(EXT_URI, '', $url);
-    if ( $pos === FALSE ) {
-        return '/'.$url.'/'.$key.'-'.$value.EXT_URI;
+    if ($pos === FALSE) {
+        return '/' . $url . '/' . $key . '-' . $value . EXT_URI;
     } else {
-        return '/'.$url.'-'.$key.'-'.$value.EXT_URI;
+        return '/' . $url . '-' . $key . '-' . $value . EXT_URI;
     }
 
 }
@@ -251,12 +263,13 @@ function addUrlArgs($url, $key, $value) {
  * @param $key
  * @return mixed
  */
-function getConfig($key) {
+function getConfig($key)
+{
 
-    if( defined('ENV_CFG') ) {
-        $appConfigs = \herosphp\core\Loader::config('app', 'env.'.ENV_CFG);
+    if (defined('ENV_CFG')) {
+        $appConfigs = \herosphp\core\Loader::config('app', 'env.' . ENV_CFG);
     } else {
-        $appConfigs =\herosphp\core\Loader::config('app');
+        $appConfigs = \herosphp\core\Loader::config('app');
     }
     return $appConfigs[$key];
 }
@@ -265,29 +278,33 @@ function getConfig($key) {
  * 获取应用的全不配置信息
  * @return array
  */
-function getConfigs() {
-    if( defined('ENV_CFG') ){
-        return \herosphp\core\Loader::config('app', 'env.'.ENV_CFG);
-    }else{
+function getConfigs()
+{
+    if (defined('ENV_CFG')) {
+        return \herosphp\core\Loader::config('app', 'env.' . ENV_CFG);
+    } else {
         return \herosphp\core\Loader::config('app');
     }
 }
 
 //跳转到404页面
-function page404() {
+function page404()
+{
     header("HTTP/1.0 404 Not Found!");
     die();
 }
 
 //转跳301页面
-function page301( $url ) {
-    header( "HTTP/1.1 301 Moved Permanently" );
-    header( "Location: {$url}" );
+function page301($url)
+{
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: {$url}");
     die();
 }
 
 //页面跳转
-function location( $url ) {
+function location($url)
+{
 
     header("Location:{$url}");
     die();
@@ -295,19 +312,22 @@ function location( $url ) {
 }
 
 //获取框架版本号
-function getFrameVersion() {
+function getFrameVersion()
+{
     return FRAME_VERSION;
 }
 
 //获取http头信息
-function getHttpHeader($key) {
-    return $_SERVER["HTTP_".str_replace('-', '_', strtoupper($key))];
+function getHttpHeader($key)
+{
+    return $_SERVER["HTTP_" . str_replace('-', '_', strtoupper($key))];
 }
 
-function getHttpHeaders() {
+function getHttpHeaders()
+{
     $headers = array();
     foreach ($_SERVER as $key => $value) {
-        if ( strpos($key, 'HTTP_') === 0 ) {
+        if (strpos($key, 'HTTP_') === 0) {
             $headers[strtolower(substr($key, 5))] = $value;
         }
     }
@@ -315,7 +335,8 @@ function getHttpHeaders() {
 }
 
 // 只允许 POST 请求方式
-function onlyPost() {
+function onlyPost()
+{
     if ($_SERVER['REQUEST_METHOD'] != 'POST') {
         header("HTTP/1.0 405 Method Not Allowed!");
         die();
@@ -323,9 +344,27 @@ function onlyPost() {
 }
 
 // 只允许 GET 请求方式
-function onlyGet() {
+function onlyGet()
+{
     if ($_SERVER['REQUEST_METHOD'] != 'GET') {
         header("HTTP/1.0 405 Method Not Allowed!");
         die();
     }
+}
+
+// get cmd args
+function getCliArgs($argv, $argc)
+{
+    $params = array('__file__' => array_shift($argv), '__url__' => array_shift($argv));
+    foreach ($argv as $v) {
+        if (strlen($v) < 3 || strncmp($v, "--", 2) != 0) {
+            continue;
+        }
+
+        if (preg_match('/--([a-zA-Z0-9]+)=([^\s]+)/', $v, $m) == 0) {
+            continue;
+        }
+        $params[$m[1]] = $m[2];
+    }
+    return $params;
 }
