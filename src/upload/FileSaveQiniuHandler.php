e81c1bb37c93821a84e531017ecd1a7ebd18c8a0
RockYang <yangjian102621@163.com>
2022-09-04 16:07:53 +0800

    add Aliyun OSS and Qiniu OSS file upload handler implement class

--- src/upload/FileSaveQiniuHandler.php
+++ src/upload/FileSaveQiniuHandler.php
@@ -10,14 +10,14 @@ declare(strict_types=1);
 
 namespace herosphp\upload;
 
-use herosphp\utils\FileUtil;
+use herosphp\exception\HeroException;
 
 /**
- * 本地文件上传 Handler 实现
+ * 七牛云 OSS 文件上传 Handler 实现
  * --------------------------------------------------
  * @author RockYang<yangjian102621@gmail.com>
  */
-class FileSaveLocalHandler implements IFileSaveHandler
+class FileSaveQiniuHandler implements IFileSaveHandler
 {
     protected array $_config = [];
 
@@ -26,31 +26,15 @@ class FileSaveLocalHandler implements IFileSaveHandler
         if ($config !== null) {
             $this->_config = $config;
         }
-
-        // create upload dir
-        if (!file_exists($this->_config['upload_dir'])) {
-            FileUtil::makeFileDirs($this->_config['upload_dir']);
-        }
     }
 
     public function save(string $srcFile, string $filename): string|false
     {
-        $dstFile = $this->_config['upload_dir'] . DIRECTORY_SEPARATOR . $filename;
-        // 此处不能用 move_uploaded_file() 函数， 不支持 Workerman 的上传协议
-        if (file_exists($srcFile) && rename($srcFile, $dstFile)) {
-            return $dstFile;
-        }
-
-        return false;
+        throw new HeroException('Not implemented.');
     }
 
     public function saveBase64($data, string $filename): string|false
     {
-        $dstFile = $this->_config['upload_dir'] . DIRECTORY_SEPARATOR . $filename;
-        if (file_put_contents($dstFile, $data)) {
-            return $dstFile;
-        }
-
-        return false;
+        throw new HeroException('Not implemented.');
     }
 }
