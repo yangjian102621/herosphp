a0aab00c3d3105f1e047c784dce3e3d8b948481c
chenzf@pvc123.com <chenzf@pvc123.com>
2022-09-04 09:15:49 +0800

    add process to herosphp

--- process/AsyncTask.php
+++ process/AsyncTask.php
@@ -9,13 +9,13 @@ use herosphp\core\Config;
 
 //需要用到什么启动器，用户自己加载
 //启动注册laravel mysql
-LaravelStarter::init(Config::get(name:'database', default: []));
+//LaravelStarter::init(Config::get(name:'database', default: []));
 
 class AsyncTask
 {
     public function run(): string
     {
-        $count = User::query()->count();
+        $count = 2;
         echo  date('Y-m-d H:i:s')."-i am running，async worker db query:{$count}".PHP_EOL;
         return 'success';
     }
